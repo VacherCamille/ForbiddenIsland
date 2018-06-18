5557c52ca13b8fc1cc15965c7ac0b488b7039e81
vacherca <vacherca@PC-DG-020-04.ntiut2gre.iut2.upmf-grenoble.fr>
2018-06-18 14:18:16 +0200

    navigateur action spe

--- IleInterditeV5/src/Modele/Aventurier/Navigateur.java
+++ IleInterditeV5/src/Modele/Aventurier/Navigateur.java
@@ -5,7 +5,9 @@
  */
 package Modele.Aventurier;
 
+import Modele.Plateau.Tuile;
 import Util.Utils;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,5 +17,16 @@ public class Navigateur extends CarteAventurier {
     
     public Navigateur() {
         super("Navigateur", Utils.Pion.JAUNE);
+    }
+    
+  
+    public void deplacerJoueur(Aventurier joueur,int ligne, int colonne) {
+        
+        if(getTuilesDeplacement().contains(joueur.getEnvironnement().getTuile(ligne, colonne))){         
+                System.out.println("\033[32m [ DEPLACEMENT EFFECTUE ! ]");
+                joueur.getPosition().setColonne(colonne);
+                joueur.getPosition().setLigne(ligne);
+                joueur.utiliserPA();
+        }
     }
 }
