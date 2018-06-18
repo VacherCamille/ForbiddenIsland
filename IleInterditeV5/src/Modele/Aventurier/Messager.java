f88fedc628a1d89a14faebad964cfa6d5d1f9dca
vacherca <vacherca@PC-DG-020-04.ntiut2gre.iut2.upmf-grenoble.fr>
2018-06-18 14:05:31 +0200

    messager action spe

--- IleInterditeV5/src/Modele/Aventurier/Messager.java
+++ IleInterditeV5/src/Modele/Aventurier/Messager.java
@@ -5,6 +5,8 @@
  */
 package Modele.Aventurier;
 
+import Modele.CarteTresor.CarteTresor;
+import Modele.Plateau.Tuile;
 import Util.Utils;
 
 /**
@@ -15,5 +17,21 @@ public class Messager extends CarteAventurier {
     
     public Messager() {
         super("Messager", Utils.Pion.ORANGE);
+    }
+    
+    public void donnerCarte(Aventurier destinataire, String nomCarte) {
+        if (destinataire.hasFullDeck()) {
+            System.out.println("\033[31m [ ERREUR DON DE CARTE : DECK DESTINATAIRE PLEIN");
+            return;
+        }
+        if (getJoueur() != null && getJoueur().getPointAction() > 0) {
+                CarteTresor carteDonnee = getJoueur().getCarteTresorFromName(nomCarte);
+                getJoueur().removeCarteTresor(carteDonnee);
+                destinataire.addCarteTresor(carteDonnee);
+                System.out.println("\033[32m [ CARTE TRANSFEREE ! ]");
+                getJoueur().utiliserPA();
+        } else {
+            System.out.println("\033[31m [ ERREUR DON DE CARTE : PAS ASSEZ DE PA ! ]");
+        }
     }
 }
